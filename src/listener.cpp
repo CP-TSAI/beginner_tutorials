23eb115b999224fb490d9b59b96adcdee6c5553c
Chin-Po <aupt23@gmail.com>
2018-10-30 21:40:18 -0400

    doxygen comment for cpp files

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,16 +1,22 @@
+/** @file listener.cpp
+ *  @brief A subscriber node that subscribes to a topic and print the contents
+ *  @copyright (c) 2018 Chin-Po Tsai
+ *  @author Chin-Po Tsai
+ */
+
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
 /**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
+ * @brief A callback function which is called when data arrives
+ * @param msg input string published by talker
+ * @return void
  */
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
-{
+void chatterCallback(const std_msgs::String::ConstPtr& msg) {
   ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line.
@@ -48,9 +54,8 @@ int main(int argc, char **argv)
   ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
 
   /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
+   * ros::spin() will enter a loop, pumping callbacks.
+   * ros::spin() will exit when Ctrl-C is pressed, or the node is shutdown by the master.
    */
   ros::spin();
 
--- src/talker.cpp
+++ src/talker.cpp
@@ -1,13 +1,18 @@
+/** @file talker.cpp
+ *  @brief A publisher node that publishes a string message
+ *  @copyright (c) 2018 Chin-Po Tsai
+ *  @author Chin-Po Tsai
+ */
+#include <sstream>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
-#include <sstream>
+
 
 /**
- * This tutorial demonstrates simple sending of messages over the ROS system.
+ * This publisher demonstrates simple sending of messages over the ROS system.
  */
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line.
@@ -53,8 +58,7 @@ int main(int argc, char **argv)
    * a unique string for each message.
    */
   int count = 0;
-  while (ros::ok())
-  {
+  while (ros::ok()) {
     /**
      * This is a message object. You stuff it with data, and then publish it.
      */
